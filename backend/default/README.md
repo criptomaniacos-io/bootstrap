c566d87b4fc2f743ae89aba83eda229a6a0f7402
Pedro Moreno <nennogabriel@gmail.com>
2023-07-15 15:36:50 -0300

    feat: one command setup

---
+++ backend/default/README.md
@@ -0,0 +1,5 @@
+# Make your setup easier with this command
+
+```sh
+curl -sSfO https://raw.githubusercontent.com/criptomaniacos-io/bootstrap/main/backend/default/setup.sh && sh setup.sh
+```
--- backend/default/setup.sh
+++ backend/default/setup.sh
@@ -2,13 +2,14 @@
 rm -rf node_modules
 npm cache clean --force
 
-curl -o .editorconfig https://gist.githubusercontent.com/isacrodriguesdev/86b5b247b25a8cd5fdf694812bf032b1/raw/.editorconfig
-curl -o .eslintrc.js https://gist.githubusercontent.com/isacrodriguesdev/7ebd934130e0a8ab83e96ee37548203e/raw/.eslintrc.js
-curl -o .prettierrc https://gist.githubusercontent.com/isacrodriguesdev/0ce9cbf81c6a5f63b97ee81bc9fd2721/raw/.prettierrc
-curl -o esbuild.js https://gist.githubusercontent.com/isacrodriguesdev/14232edc3ebb3225bdee1ed4cbbb9361/raw/esbuild.js
-curl -o tsconfig.json https://gist.githubusercontent.com/isacrodriguesdev/c5049638cce57a1dfe0e1552534565fc/raw/tsconfig.json
-curl -o .nvmrc https://gist.githubusercontent.com/isacrodriguesdev/a6a2a22c6a277b4f76be68c1ec8df889/raw/.nvmrc
-curl -o .gitignore https://gist.githubusercontent.com/isacrodriguesdev/86ebf408064a04a0b38565cc6d99cdbc/raw/.gitignore
+curl -o .editorconfig https://raw.githubusercontent.com/criptomaniacos-io/bootstrap/main/backend/default/.editorconfig
+curl -o .eslintrc.js https://raw.githubusercontent.com/criptomaniacos-io/bootstrap/main/backend/default/.eslintrc.js
+curl -o .prettierrc https://raw.githubusercontent.com/criptomaniacos-io/bootstrap/main/backend/default/.prettierrc
+curl -o esbuild.js https://raw.githubusercontent.com/criptomaniacos-io/bootstrap/main/backend/default/esbuild.js
+curl -o tsconfig.json https://raw.githubusercontent.com/criptomaniacos-io/bootstrap/main/backend/default/tsconfig.json 
+curl -o .nvmrc https://raw.githubusercontent.com/criptomaniacos-io/bootstrap/main/backend/default/.nvmrc
+curl -o .npmrc https://raw.githubusercontent.com/criptomaniacos-io/bootstrap/main/backend/default/.npmrc
+curl -o .gitignore https://raw.githubusercontent.com/criptomaniacos-io/bootstrap/main/backend/default/.gitignore 
 
 npm install --save-dev eslint eslint-plugin-import@latest @typescript-eslint/eslint-plugin @typescript-eslint/parser @typescript-eslint/typescript-estree eslint-config-prettier eslint-plugin-prettier prettier tsconfig-paths typescript@4.9.5 esbuild
 
